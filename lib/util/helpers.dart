50f85196444cae72a05adf25ee0a416c5a62b131
Abdullah Emad <amenesy8@gmail.com>
2023-12-05 10:44:55 +0200

    making helpers methods as static in class make it more readable and make code more cleaner

--- lib/util/helpers.dart
+++ lib/util/helpers.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-void showProgressInidcator(BuildContext context) {
-  AlertDialog alertDialog = const AlertDialog(
-    backgroundColor: Colors.transparent,
-    elevation: 0,
-    content: Center(
-      child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+class Helpers {
+  static void showProgressInidcator(BuildContext context) {
+    AlertDialog alertDialog = const AlertDialog(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      content: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+        ),
       ),
-    ),
-  );
+    );
 
-  showDialog(
-    barrierColor: Colors.white.withOpacity(0),
-    barrierDismissible: false,
-    context: context,
-    builder: (context) {
-      return alertDialog;
-    },
-  );
+    showDialog(
+      barrierColor: Colors.white.withOpacity(0),
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return alertDialog;
+      },
+    );
+  }
+
+  static Future<Position> detectCurrentLocation() async {
+    bool isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) {
+      await Geolocator.requestPermission();
+    }
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
 }
